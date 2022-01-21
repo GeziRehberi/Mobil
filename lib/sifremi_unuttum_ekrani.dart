9eec12ac5b466695d917aeea72f48e525159641d
nuriulgen <bazid04ulgen@gmail.com>
2022-01-21 15:26:43 +0300

    closes #30
    email adresinizi doğrulayınız giriş alanı oluşturuldu ve email doğrulamas işlemleri yapıldı

--- lib/sifremi_unuttum_ekrani.dart
+++ lib/sifremi_unuttum_ekrani.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 
 class SifremiUnutttumEkrani extends StatelessWidget {
-  const SifremiUnutttumEkrani({Key? key}) : super(key: key);
+  SifremiUnutttumEkrani({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -57,6 +57,20 @@ class SifremiUnutttumEkrani extends StatelessWidget {
                 ]),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40)),
+                    hintText: 'Email adresinizi doğrulayınız'),
+                validator: MultiValidator([
+                  RequiredValidator(errorText: "E-mail girmek zorunlu"),
+                  EmailValidator(errorText: "Geçerli bir email giriniz:"),
+                ]),
+              ),
+            ),
+            SizedBox(height: 30),
           ],
         ),
       ),
