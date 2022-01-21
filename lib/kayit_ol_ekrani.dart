f2b01187a8bee0e383c215cc33304ba7d8b749af
nuriulgen <bazid04ulgen@gmail.com>
2022-01-21 03:59:42 +0300

    closes #23
    ad giriş alanı eklendi ve bir çok özellik eklemdi

--- lib/kayit_ol_ekrani.dart
+++ lib/kayit_ol_ekrani.dart
@@ -1,45 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class KayitOlEkrani extends StatelessWidget {
-  const KayitOlEkrani({Key? key}) : super(key: key);
+  KayitOlEkrani({Key? key}) : super(key: key);
+  final FocusNode focusName = FocusNode();
 
+  final TextEditingController nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
+      appBar: AppBar(title: Text('Hesap Oluştur')),
+      body: Center(
+          child: Container(
+        padding: EdgeInsets.only(top: 50.0),
         child: Column(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 130),
-              child: Center(
-                child: Container(
-                  width: 180,
-                  height: 43,
-                  child: Center(
-                    child: Text(
-                      'Gezi Rehberi',
-                      style: TextStyle(
-                        fontSize: 22,
-                      ),
+            Stack(
+              alignment: Alignment.topCenter,
+              overflow: Overflow.visible,
+              children: <Widget>[
+                Card(
+                  elevation: 2.0,
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0)),
+                  child: Container(
+                    width: 360.00,
+                    height: 580.00,
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(
+                              top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
+                          child: TextField(
+                            focusNode: focusName,
+                            controller: nameController,
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(
+                                fontFamily: "SignikaSemiBold",
+                                fontSize: 16.0,
+                                color: Colors.black),
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                icon: Icon(
+                                  FontAwesomeIcons.user,
+                                  color: Colors.black,
+                                  size: 22.0,
+                                ),
+                                hintText: "Adınızı giriniz",
+                                hintStyle: TextStyle(
+                                    fontFamily: "SignikaSemiBold",
+                                    fontSize: 18.0)),
+                          ),
+                        ),
+                        Container(
+                          width: 250.0,
+                          height: 1.0,
+                          color: Colors.grey,
+                        ),
+                      ],
                     ),
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.indigoAccent,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              child: Image.asset(
-                'assets/konum.png',
-                scale: 1,
-              ),
+              ],
             ),
           ],
         ),
-      ),
+      )),
     );
   }
 }
