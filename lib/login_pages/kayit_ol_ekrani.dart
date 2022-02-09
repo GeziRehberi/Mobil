ac361c154f56f9372446c56e01549e471fcfdec7
nuriulgen <bazid04ulgen@gmail.com>
2022-02-09 21:08:11 +0300

    ad giriş alanı güncellendi closes #23
    Ad giriş alanı ve validate işlemleri güncellendi

--- lib/login_pages/kayit_ol_ekrani.dart
+++ lib/login_pages/kayit_ol_ekrani.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 class KayitOlEkrani extends StatelessWidget {
   KayitOlEkrani({Key? key}) : super(key: key);
 
+  String _ad = '';
+  String _soyAd = '';
+  String _email = '';
+  String _sifre = '';
+
+  final _formkey3 = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: Color(0xFF3949AB),
+        ),
         title: const Text(
           'Hesap Oluştur',
           style: TextStyle(color: Colors.black),
@@ -31,8 +42,39 @@ class KayitOlEkrani extends StatelessWidget {
                       width: 390.00,
                       height: 550.00,
                       child: Form(
+                        key: _formkey3,
                         child: Column(
-                          children: <Widget>[],
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 60.0,
+                                  bottom: 20.0,
+                                  left: 25.0,
+                                  right: 25.0),
+                              child: TextFormField(
+                                  autovalidateMode:
+                                      AutovalidateMode.onUserInteraction,
+                                  keyboardType: TextInputType.emailAddress,
+                                  style: const TextStyle(
+                                      fontSize: 16.0, color: Colors.black),
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(40),
+                                    ),
+                                    hintText: 'Adınızı giriniz',
+                                  ),
+                                  validator: (deger) {
+                                    if (deger!.length < 3) {
+                                      return 'Adınız en az 3 karakter olmalı';
+                                    } else {
+                                      return null;
+                                    }
+                                  },
+                                  onSaved: (deger) {
+                                    _ad = deger!;
+                                  }),
+                            ),
+                          ],
                         ),
                       ),
                     ),
