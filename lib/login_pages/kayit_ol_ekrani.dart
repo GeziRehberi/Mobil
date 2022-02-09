d964436272759291b2bfb7b903b19875e7f19c13
nuriulgen <bazid04ulgen@gmail.com>
2022-02-09 21:22:02 +0300

    Emaiş giriş alanı güncellendi closes #25
    Email giriş alanı ve validate işlemleri güncellendi

--- lib/login_pages/kayit_ol_ekrani.dart
+++ lib/login_pages/kayit_ol_ekrani.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 
 class KayitOlEkrani extends StatelessWidget {
@@ -99,6 +100,40 @@ class KayitOlEkrani extends StatelessWidget {
                                 validator: (deger) {
                                   if (deger!.length < 2) {
                                     return 'Soyadınız en az 2 karakter olmalı';
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 10.0,
+                                  bottom: 20.0,
+                                  left: 25.0,
+                                  right: 25.0),
+                              child: TextFormField(
+                                autovalidateMode:
+                                    AutovalidateMode.onUserInteraction,
+                                keyboardType: TextInputType.emailAddress,
+                                style: const TextStyle(
+                                    fontSize: 16.0, color: Colors.black),
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(40),
+                                  ),
+                                  hintText: 'Email giriniz',
+                                ),
+                                onSaved: (deger) {
+                                  _email = deger!; // verilerin kaydedilmesi
+                                  print(_email);
+                                },
+                                validator: (deger) {
+                                  // email doğrulama işlemleri
+                                  if (deger!.isEmpty) {
+                                    return 'mail boş olamaz';
+                                  } else if (!EmailValidator.validate(deger)) {
+                                    return 'Geçerli bir mail giriniz';
                                   } else {
                                     return null;
                                   }
