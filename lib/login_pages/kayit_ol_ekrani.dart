6e3d9c3b4b09de8c494d2a9a3327e4b2f4de05cd
nuriulgen <bazid04ulgen@gmail.com>
2022-02-09 20:49:00 +0300

    kayit ol ekranı güncellendi closes #20
    Kayıt ol ekranı yeniden tasarlandı

--- lib/login_pages/kayit_ol_ekrani.dart
+++ lib/login_pages/kayit_ol_ekrani.dart
@@ -1,232 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import '../models/theme.dart';
-import 'giris_yap_ekrani.dart';
 
 class KayitOlEkrani extends StatelessWidget {
   KayitOlEkrani({Key? key}) : super(key: key);
 
-  final FocusNode focusName = FocusNode();
-  final FocusNode focusSurName = FocusNode();
-  final FocusNode focusEmail = FocusNode();
-  final FocusNode focusPassword = FocusNode();
-  final FocusNode focusConfirmPassword = FocusNode();
-
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController surnameController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController =
-      TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Hesap Oluştur')),
-      body: Center(
-          child: Container(
-        padding: EdgeInsets.only(top: 50.0),
-        child: Column(
-          children: <Widget>[
-            Stack(
-              alignment: Alignment.topCenter,
-              overflow: Overflow.visible,
-              children: <Widget>[
-                Card(
-                  elevation: 2.0,
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0)),
-                  child: Container(
-                    width: 360.00,
-                    height: 580.00,
-                    child: Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(
-                              top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
-                          child: TextField(
-                            focusNode: focusName,
-                            controller: nameController,
-                            keyboardType: TextInputType.emailAddress,
-                            style: TextStyle(
-                                fontFamily: "SignikaSemiBold",
-                                fontSize: 16.0,
-                                color: Colors.black),
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                icon: Icon(
-                                  FontAwesomeIcons.user,
-                                  color: Colors.black,
-                                  size: 22.0,
-                                ),
-                                hintText: "Adınızı giriniz",
-                                hintStyle: TextStyle(
-                                    fontFamily: "SignikaSemiBold",
-                                    fontSize: 18.0)),
-                          ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Hesap Oluştur',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+            child: Container(
+          padding: const EdgeInsets.only(top: 40.0),
+          child: Column(
+            children: <Widget>[
+              Stack(
+                alignment: Alignment.topCenter,
+                children: <Widget>[
+                  Card(
+                    elevation: 2.0,
+                    color: Colors.white.withOpacity(0.9),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    child: Container(
+                      width: 390.00,
+                      height: 550.00,
+                      child: Form(
+                        child: Column(
+                          children: <Widget>[],
                         ),
-                        Container(
-                            width: 250.0, height: 1.0, color: Colors.grey),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
-                          child: TextField(
-                            focusNode: focusSurName,
-                            controller: surnameController,
-                            keyboardType: TextInputType.emailAddress,
-                            style: TextStyle(
-                                fontFamily: "SignikaSemiBold",
-                                fontSize: 16.0,
-                                color: Colors.black),
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                icon: Icon(
-                                  FontAwesomeIcons.user,
-                                  color: Colors.black,
-                                  size: 22.0,
-                                ),
-                                hintText: "Soyadınızı giriniz",
-                                hintStyle: TextStyle(
-                                    fontFamily: "SignikaSemiBold",
-                                    fontSize: 18.0)),
-                          ),
-                        ),
-                        Container(
-                            width: 250.0, height: 1.0, color: Colors.grey),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
-                          child: TextField(
-                            focusNode: focusEmail,
-                            controller: emailController,
-                            keyboardType: TextInputType.emailAddress,
-                            style: TextStyle(
-                                fontFamily: "SignikaSemiBold",
-                                fontSize: 16.0,
-                                color: Colors.black),
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                icon: Icon(
-                                  FontAwesomeIcons.envelope,
-                                  color: Colors.black,
-                                  size: 22.0,
-                                ),
-                                hintText: "Email adresinizi giriniz",
-                                hintStyle: TextStyle(
-                                    fontFamily: "SignikaSemiBold",
-                                    fontSize: 18.0)),
-                          ),
-                        ),
-                        Container(
-                            width: 250.0, height: 1.0, color: Colors.grey),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
-                          child: TextField(
-                            focusNode: focusPassword,
-                            controller: passwordController,
-                            style: TextStyle(
-                                fontFamily: "SignikaSemiBold",
-                                fontSize: 16.0,
-                                color: Colors.black),
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                icon: Icon(
-                                  FontAwesomeIcons.lock,
-                                  color: Colors.black,
-                                  size: 22.0,
-                                ),
-                                hintText: "Şifrenizi giriniz",
-                                hintStyle: TextStyle(
-                                    fontFamily: "SignikaSemiBold",
-                                    fontSize: 18.0)),
-                          ),
-                        ),
-                        Container(
-                          width: 250.0,
-                          height: 1.0,
-                          color: Colors.grey,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              top: 20.0, bottom: 20.0, left: 25.0, right: 25.0),
-                          child: TextField(
-                            focusNode: focusPassword,
-                            controller: passwordController,
-                            style: TextStyle(
-                                fontFamily: "SignikaSemiBold",
-                                fontSize: 16.0,
-                                color: Colors.black),
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                icon: Icon(
-                                  FontAwesomeIcons.lock,
-                                  color: Colors.black,
-                                  size: 22.0,
-                                ),
-                                hintText: "Şifrenizi tekrar giriniz",
-                                hintStyle: TextStyle(
-                                    fontFamily: "SignikaSemiBold",
-                                    fontSize: 18.0)),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 40.0),
-                          decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5.0)),
-                              boxShadow: <BoxShadow>[
-                                BoxShadow(
-                                    color: AppColours.colorStart,
-                                    offset: Offset(1.0, 6.0),
-                                    blurRadius: 20.0),
-                                BoxShadow(
-                                    color: AppColours.colorEnd,
-                                    offset: Offset(1.0, 6.0),
-                                    blurRadius: 20.0),
-                              ],
-                              gradient: LinearGradient(
-                                  colors: [
-                                    AppColours.colorEnd,
-                                    AppColours.colorStart
-                                  ],
-                                  begin: const FractionalOffset(0.2, 0.2),
-                                  end: const FractionalOffset(1.0, 1.0),
-                                  stops: [0.1, 1.0],
-                                  tileMode: TileMode.clamp)),
-                          child: MaterialButton(
-                            highlightColor: Colors.transparent,
-                            splashColor: AppColours.colorEnd,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 10.0, horizontal: 42.0),
-                              child: Text(
-                                "Kayıt Ol",
-                                style: TextStyle(
-                                    fontFamily: "SignikaSemiBold",
-                                    color: Colors.white,
-                                    fontSize: 22.0),
-                              ),
-                            ),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (_) => GirisYapEkrani()));
-                            },
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      )),
+                ],
+              ),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
