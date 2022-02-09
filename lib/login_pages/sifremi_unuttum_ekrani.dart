c875860e0656dcc0ff916e6a13a08a0a7d167ae2
nuriulgen <bazid04ulgen@gmail.com>
2022-02-10 00:50:29 +0300

    Doğrulama kodu butonu güncellendi
    Doğrulama kodu butonu  ve validate işlemleri güncellendi.

--- lib/login_pages/sifremi_unuttum_ekrani.dart
+++ lib/login_pages/sifremi_unuttum_ekrani.dart
@@ -1,4 +1,5 @@
 import 'package:email_validator/email_validator.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'dogrulama_kodu_ekrani.dart';
@@ -12,6 +13,10 @@ class SifremiUnutttumEkrani extends StatefulWidget {
 
 class _SifremiUnutttumEkraniState extends State<SifremiUnutttumEkrani> {
   String _email = '';
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _confirmEmailController = TextEditingController();
+
   final _formkey2 = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -64,7 +69,9 @@ class _SifremiUnutttumEkraniState extends State<SifremiUnutttumEkrani> {
                       padding:
                           const EdgeInsets.only(left: 16, right: 16, top: 40),
                       child: TextFormField(
+                        controller: _emailController,
                         autovalidateMode: AutovalidateMode.onUserInteraction,
+                        keyboardType: TextInputType.emailAddress,
                         decoration: InputDecoration(
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(40)),
@@ -89,7 +96,9 @@ class _SifremiUnutttumEkraniState extends State<SifremiUnutttumEkrani> {
                       padding:
                           const EdgeInsets.only(left: 16, right: 16, top: 10),
                       child: TextFormField(
+                        controller: _confirmEmailController,
                         autovalidateMode: AutovalidateMode.onUserInteraction,
+                        keyboardType: TextInputType.emailAddress,
                         decoration: InputDecoration(
                             border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(40)),
@@ -104,6 +113,9 @@ class _SifremiUnutttumEkraniState extends State<SifremiUnutttumEkrani> {
                             return 'mail boş olamaz';
                           } else if (!EmailValidator.validate(deger)) {
                             return 'Geçerli bir mail giriniz';
+                          } else if (_emailController.text !=
+                              _confirmEmailController.text) {
+                            return "Mailler eşleşmiyor!";
                           } else {
                             return null;
                           }
@@ -119,10 +131,27 @@ class _SifremiUnutttumEkraniState extends State<SifremiUnutttumEkrani> {
               decoration: BoxDecoration(
                   color: Color(0xFF3949AB),
                   borderRadius: BorderRadius.circular(20)),
-              child: OutlinedButton(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: const Color(0xFF3949AB),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)),
+                ),
                 onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => KoduDogrula()));
+                  bool _validate = _formkey2.currentState!.validate();
+                  if (_validate) {
+                    _formkey2.currentState!.save();
+                    // bilgileri okuyup ekranda göstermesi
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                        'Kod mail adresinize gönderilmiştir',
+                      ),
+                    ));
+                    Navigator.pushReplacement(
+                        context,
+                        CupertinoPageRoute(
+                            builder: (context) => KoduDogrula()));
+                  }
                 },
                 child: Text(
                   'Doğrulama Kodu Gönder',
