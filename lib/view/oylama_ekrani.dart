d740577fadbe98d35f757210173f10e9b19fe1f2
nuriulgen <bazid04ulgen@gmail.com>
2022-02-14 03:42:47 +0300

    initial commit

--- lib/view/oylama_ekrani.dart
+++ lib/view/oylama_ekrani.dart
@@ -1,1 +1,22 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class OylamaSayfasi extends StatefulWidget {
+  const OylamaSayfasi({Key? key}) : super(key: key);
+
+  @override
+  _OylamaSayfasiState createState() => _OylamaSayfasiState();
+}
+
+class _OylamaSayfasiState extends State<OylamaSayfasi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Oylama'),
+      ),
+      body: Center(
+        child: Text('Oylama'),
+      ),
+    );
+  }
+}
