e96f4938fbca03988bdf7d85864b578361fcad59
nuriulgen <bazid04ulgen@gmail.com>
2022-02-14 05:26:12 +0300

    Takip,takipçiler ve yorumlarım arayüz oluşturuldu closes #59
    Takip,takipçiler ve yorumlarım kullanıcı arayüzü oluşturuldu

--- lib/view/profil_ekrani.dart
+++ lib/view/profil_ekrani.dart
@@ -37,6 +37,8 @@ class _ProfilSayfasiState extends State<ProfilSayfasi> {
           ),
           const SizedBox(height: 24),
           buildName(user),
+          const SizedBox(height: 24),
+          NumbersWidget(),
         ],
       ),
     );
@@ -126,6 +128,46 @@ class ProfileWidget extends StatelessWidget {
           padding: EdgeInsets.all(all),
           color: color,
           child: child,
+        ),
+      );
+}
+
+class NumbersWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          buildButton(context, '13', 'Yorumlarım'),
+          buildDivider(),
+          buildButton(context, '35', 'Takip'),
+          buildDivider(),
+          buildButton(context, '82', 'Takipçiler'),
+        ],
+      );
+  Widget buildDivider() => Container(
+        height: 32,
+        child: VerticalDivider(),
+      );
+
+  Widget buildButton(BuildContext context, String value, String text) =>
+      MaterialButton(
+        padding: EdgeInsets.symmetric(vertical: 4),
+        onPressed: () {},
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              value,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            SizedBox(height: 2),
+            Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       );
 }
