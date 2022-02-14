b14cff9810aa33bc1662b40d576600260cdbdc8a
nuriulgen <bazid04ulgen@gmail.com>
2022-02-14 05:42:38 +0300

    edit_profile_page sayfası oluşturuldu closes #60
    edit_profile_page sayfası oluşturuldu. Bu sayfa profil resmi,kullanıcı adı  ve email güncelleme işlemleri yapıldı

---
+++ lib/pages/edit_profile_page.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_widgets/model/user.dart';
+import 'package:flutter_widgets/model/user_preferences.dart';
+
+class EditProfilePage extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  User user = UserPreferences.myUser;
+  final icon = CupertinoIcons.moon_stars;
+  @override
+  Widget build(BuildContext context) => ThemeSwitchingArea(
+        child: Builder(
+          builder: (context) => Scaffold(
+            appBar: AppBar(
+              leading: BackButton(),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: [
+                IconButton(
+                  icon: Icon(icon, color: Colors.black, size: 30),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            body: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              physics: BouncingScrollPhysics(),
+              children: [
+                ProfileWidget(
+                  imagePath: user.imagePath,
+                  isEdit: true,
+                  onClicked: () async {},
+                ),
+                const SizedBox(height: 24),
+                TextFieldWidget(
+                  label: 'Full Name',
+                  text: user.name,
+                  onChanged: (name) {},
+                ),
+                const SizedBox(height: 24),
+                TextFieldWidget(
+                  label: 'Email',
+                  text: user.email,
+                  onChanged: (email) {},
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+}
+
+class ProfileWidget extends StatelessWidget {
+  final String imagePath;
+  final bool isEdit;
+  final VoidCallback onClicked;
+
+  const ProfileWidget({
+    Key? key,
+    required this.imagePath,
+    this.isEdit = false,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme.primary;
+
+    return Center(
+      child: Stack(
+        children: [
+          buildImage(),
+          Positioned(
+            bottom: 0,
+            right: 4,
+            child: buildEditIcon(color),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildImage() {
+    final image = NetworkImage(imagePath);
+
+    return ClipOval(
+      child: Material(
+        color: Colors.transparent,
+        child: Ink.image(
+          image: image,
+          fit: BoxFit.cover,
+          width: 128,
+          height: 128,
+          child: InkWell(onTap: onClicked),
+        ),
+      ),
+    );
+  }
+
+  Widget buildEditIcon(Color color) => buildCircle(
+        color: Colors.white,
+        all: 3,
+        child: buildCircle(
+          color: color,
+          all: 8,
+          child: Icon(
+            isEdit ? Icons.add_a_photo : Icons.edit,
+            color: Colors.white,
+            size: 20,
+          ),
+        ),
+      );
+
+  Widget buildCircle({
+    required Widget child,
+    required double all,
+    required Color color,
+  }) =>
+      ClipOval(
+        child: Container(
+          padding: EdgeInsets.all(all),
+          color: color,
+          child: child,
+        ),
+      );
+}
+
+class TextFieldWidget extends StatefulWidget {
+  final int maxLines;
+  final String label;
+  final String text;
+  final ValueChanged<String> onChanged;
+
+  const TextFieldWidget({
+    Key? key,
+    this.maxLines = 1,
+    required this.label,
+    required this.text,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  _TextFieldWidgetState createState() => _TextFieldWidgetState();
+}
+
+class _TextFieldWidgetState extends State<TextFieldWidget> {
+  late final TextEditingController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = TextEditingController(text: widget.text);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.label,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          const SizedBox(height: 8),
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            maxLines: widget.maxLines,
+          ),
+        ],
+      );
+}
