1ba11d278ee2b6ead590b024c2b520e00d032dd0
nuriulgen <bazid04ulgen@gmail.com>
2022-02-15 02:23:48 +0300

    Çıkış yap butonu güncellendi closes #40
    Çıkış yap butonu oluşturuldu ve alert dialog ile eklenerek çıkış yap işlemleri eklendi

--- lib/view/profil_ekrani.dart
+++ lib/view/profil_ekrani.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/login_pages/giris_yap_ekrani.dart';
 import 'package:flutter_widgets/model/user.dart';
 import 'package:flutter_widgets/model/user_preferences.dart';
 import 'package:flutter_widgets/pages/edit_profile_page.dart';
@@ -17,141 +18,177 @@ class _ProfilSayfasiState extends State<ProfilSayfasi> {
   @override
   Widget build(BuildContext context) {
     final user = UserPreferences.myUser;
-    return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(icon, color: Colors.black, size: 30),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: ListView(
-          physics: BouncingScrollPhysics(),
-          children: [
-            ProfileWidget(
-              imagePath: user.imagePath,
-              onClicked: () {
-                Navigator.push(
-                    context,
-                    CupertinoPageRoute(
-                        builder: (context) => EditProfilePage()));
-              },
-            ),
-            const SizedBox(height: 24),
-            buildName(user),
-            const SizedBox(height: 20),
-            NumbersWidget(),
-            const SizedBox(height: 15),
-            ListTile(
-              leading: const CircleAvatar(
-                backgroundColor: Colors.green,
-                child: Icon(CupertinoIcons.person),
-              ),
-              title: const Text(
-                'Account Settings',
-                style: TextStyle(color: Colors.black),
-              ),
-              subtitle: const Text(
-                'Privacy,Language',
-                style: TextStyle(color: Colors.black, fontSize: 11),
-              ),
-              trailing: Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.transparent,
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.arrow_forward_ios,
-                      size: 17, color: Colors.black),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            ListTile(
-              title: const Text(
-                'Send Feedback',
-                style: TextStyle(color: Colors.black),
-              ),
-              leading: const CircleAvatar(
-                backgroundColor: Colors.purpleAccent,
-                child: Icon(Icons.messenger_outlined),
-              ),
-              trailing: Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.transparent,
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.arrow_forward_ios,
-                      size: 17, color: Colors.black),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            ListTile(
-              title: const Text(
-                'Report a Bug',
-                style: TextStyle(color: Colors.black),
-              ),
-              leading: const CircleAvatar(
-                backgroundColor: Colors.teal,
-                child: Icon(Icons.bug_report),
-              ),
-              trailing: Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.transparent,
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.arrow_forward_ios,
-                      size: 17, color: Colors.black),
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-            ListTile(
-              title: const Text(
-                'Logout',
-                style: TextStyle(color: Colors.black),
-              ),
-              leading: const CircleAvatar(
-                backgroundColor: Colors.blue,
-                child: Icon(Icons.logout),
-              ),
-              trailing: Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.transparent,
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.arrow_forward_ios,
-                      size: 17, color: Colors.black),
-                ),
-              ),
+    return WillPopScope(
+      onWillPop: () async {
+        final shouldPop = await showMyDialog(context);
+        return shouldPop ?? false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: BackButton(),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          actions: [
+            IconButton(
+              icon: Icon(icon, color: Colors.black, size: 30),
+              onPressed: () {},
             ),
           ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: [
+              ProfileWidget(
+                imagePath: user.imagePath,
+                onClicked: () {
+                  Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                          builder: (context) => EditProfilePage()));
+                },
+              ),
+              const SizedBox(height: 24),
+              buildName(user),
+              const SizedBox(height: 20),
+              NumbersWidget(),
+              const SizedBox(height: 15),
+              ListTile(
+                leading: const CircleAvatar(
+                  backgroundColor: Colors.green,
+                  child: Icon(CupertinoIcons.person),
+                ),
+                title: const Text(
+                  'Account Settings',
+                  style: TextStyle(color: Colors.black),
+                ),
+                subtitle: const Text(
+                  'Privacy,Language',
+                  style: TextStyle(color: Colors.black, fontSize: 11),
+                ),
+                trailing: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.transparent,
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_forward_ios,
+                        size: 17, color: Colors.black),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              ListTile(
+                title: const Text(
+                  'Send Feedback',
+                  style: TextStyle(color: Colors.black),
+                ),
+                leading: const CircleAvatar(
+                  backgroundColor: Colors.purpleAccent,
+                  child: Icon(Icons.messenger_outlined),
+                ),
+                trailing: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.transparent,
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_forward_ios,
+                        size: 17, color: Colors.black),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              ListTile(
+                title: const Text(
+                  'Report a Bug',
+                  style: TextStyle(color: Colors.black),
+                ),
+                leading: const CircleAvatar(
+                  backgroundColor: Colors.teal,
+                  child: Icon(Icons.bug_report),
+                ),
+                trailing: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.transparent,
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.arrow_forward_ios,
+                        size: 17, color: Colors.black),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              ListTile(
+                title: const Text(
+                  'Logout',
+                  style: TextStyle(color: Colors.black),
+                ),
+                leading: const CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: Icon(Icons.logout),
+                ),
+                trailing: Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.transparent,
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      showMyDialog(context);
+                    },
+                    icon: Icon(Icons.arrow_forward_ios,
+                        size: 17, color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Future<bool?> showMyDialog(BuildContext context) => showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Emin Misin?'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, false);
+                  },
+                  child: const Text('Hayır')),
+              TextButton(
+                  onPressed: () {
+                    //Giriş yap sayfasına yönlendirir ve geriye dönmesini engeller
+                    Navigator.of(context).pushAndRemoveUntil(
+                      CupertinoPageRoute(
+                          builder: (context) => GirisYapEkrani()),
+                      (_) => false,
+                    );
+                    /*SystemChannels.platform.invokeMethod(
+                        'SystemNavigator.pop');*/ //evet butonuna basınca direkt uygulamadan çıkış yapıyor
+                  },
+                  child: const Text('Evet')),
+            ],
+          );
+        },
+      );
 }
 
 Widget buildName(User user) => Column(
