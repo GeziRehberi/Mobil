a4abe49527578336ed281a8687b9a0c1b56eb9a1
nuriulgen <bazid04ulgen@gmail.com>
2022-02-14 05:52:24 +0300

    Update user.dart

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,14 +1,12 @@
 class User {
   final String imagePath;
   final String name;
-  final String surName;
   final String email;
   final bool isDarkMode;
 
   const User({
     required this.imagePath,
     required this.name,
-    required this.surName,
     required this.email,
     required this.isDarkMode,
   });
