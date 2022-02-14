220219128bd9ed576a3026bfbcd436a680f95e9e
nuriulgen <bazid04ulgen@gmail.com>
2022-02-14 04:43:50 +0300

    Update UserPreferences

--- lib/model/user_preferences.dart
+++ lib/model/user_preferences.dart
@@ -2,10 +2,11 @@ import 'package:flutter_widgets/model/user.dart';
 
 class UserPreferences {
   static const myUser = User(
-    imagePath: 'https://pbs.twimg.com/profile_images/1408322029061824512/7oNDK2Tb_400x400.jpg',
+    imagePath:
+        'https://pbs.twimg.com/profile_images/1408322029061824512/7oNDK2Tb_400x400.jpg',
     name: 'Nuri',
     surName: 'Ülgen',
     email: 'nuriulgen07@gmail.com',
     isDarkMode: false,
   );
-
+}
