074befe5b29ebeee7c59779618e3e3b9ff49b5fb
nuriulgen <bazid04ulgen@gmail.com>
2022-02-15 18:08:57 +0300

    verileri kaydetme eklendi  closes
    profi resmi,kullanıcı adı ve email güncelleme işlemleri yapıldı ve kaydedilen değerleri dinamik olarak arayüze yansıtıldı

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -1,10 +1,14 @@
-import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/model/user.dart';
 import 'package:flutter_widgets/model/user_preferences.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
-class EditProfilePage extends StatefulWidget {
+/*class EditProfilePage extends StatefulWidget {
   @override
   _EditProfilePageState createState() => _EditProfilePageState();
 }
@@ -53,6 +57,106 @@ class _EditProfilePageState extends State<EditProfilePage> {
           ),
         ),
       );
+}*/
+
+class EditProfilePage extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  late User user;
+  final icon = CupertinoIcons.moon_stars;
+  @override
+  void initState() {
+    super.initState();
+
+    user = UserPreferences.getUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Icon(icon, color: Colors.black, size: 30),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 32),
+        physics: BouncingScrollPhysics(),
+        children: [
+          ProfileWidget(
+            imagePath: user.imagePath,
+            isEdit: true,
+            onClicked: () async {
+              final image =
+                  await ImagePicker().getImage(source: ImageSource.gallery);
+
+              if (image == null) return;
+
+              final directory = await getApplicationDocumentsDirectory();
+              final name = basename(image.path);
+              final imageFile = File('${directory.path}/$name');
+              final newImage = await File(image.path).copy(imageFile.path);
+
+              setState(() => user = user.copy(imagePath: newImage.path));
+            },
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            label: 'Full Name',
+            text: user.name,
+            onChanged: (name) => user = user.copy(name: name),
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            label: 'Email',
+            text: user.email,
+            onChanged: (email) => user = user.copy(email: email),
+          ),
+          const SizedBox(height: 24),
+          ButtonWidget(
+            text: 'Save',
+            onClicked: () {
+              UserPreferences.setUser(user);
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWidget extends StatelessWidget {
+  final String text;
+  final VoidCallback onClicked;
+
+  const ButtonWidget({
+    Key? key,
+    required this.text,
+    required this.onClicked,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: StadiumBorder(),
+          onPrimary: Colors.white,
+          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
+        ),
+        child: Text(text),
+        onPressed: onClicked,
+      );
 }
 
 class ProfileWidget extends StatelessWidget {
@@ -86,13 +190,15 @@ class ProfileWidget extends StatelessWidget {
   }
 
   Widget buildImage() {
-    final image = NetworkImage(imagePath);
+    final image = imagePath.contains('https://')
+        ? NetworkImage(imagePath)
+        : FileImage(File(imagePath));
 
     return ClipOval(
       child: Material(
         color: Colors.transparent,
         child: Ink.image(
-          image: image,
+          image: image as ImageProvider,
           fit: BoxFit.cover,
           width: 128,
           height: 128,
@@ -182,6 +288,7 @@ class _TextFieldWidgetState extends State<TextFieldWidget> {
               ),
             ),
             maxLines: widget.maxLines,
+            onChanged: widget.onChanged,
           ),
         ],
       );
