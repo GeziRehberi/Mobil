dcfecfbac8ec25c6ad9d2e1f76e508237747aa8a
nuriulgen <bazid04ulgen@gmail.com>
2022-02-16 20:06:14 +0300

    veriler dynamic olarak güncelle işlemleri yapıldı
    json verileri dart nesnesine donüştürme işlemleri yapıldı

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -10,4 +10,30 @@ class User {
     required this.email,
     required this.isDarkMode,
   });
+  User copy({
+    String? imagePath,
+    String? name,
+    String? email,
+    bool? isDarkMode,
+  }) =>
+      User(
+        imagePath: imagePath ?? this.imagePath,
+        name: name ?? this.name,
+        email: email ?? this.email,
+        isDarkMode: isDarkMode ?? this.isDarkMode,
+      );
+
+  static User fromJson(Map<String, dynamic> json) => User(
+        imagePath: json['imagePath'],
+        name: json['name'],
+        email: json['email'],
+        isDarkMode: json['isDarkMode'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'imagePath': imagePath,
+        'name': name,
+        'email': email,
+        'isDarkMode': isDarkMode,
+      };
 }
