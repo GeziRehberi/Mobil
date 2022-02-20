3abe1ec00fb51e057eeb427b0c0b0367b4d27b65
nuriulgen <bazid04ulgen@gmail.com>
2022-02-20 16:32:20 +0300

    Favori mekanlar bölümü oluşturuldu.
    Favori mekanlar kısmı card widget ile listelendi

--- lib/view/profil_ekrani.dart
+++ lib/view/profil_ekrani.dart
@@ -286,6 +286,116 @@ class _ProfilSayfasiState extends State<ProfilSayfasi>
                     ),
                   ),
                   //favori mekanlar bölümü
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(),
+                          child: Card(
+                            elevation: 5,
+                            shadowColor: Colors.grey,
+                            color: Colors.white,
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    'https://i4.hurimg.com/i/hurriyet/75/0x0/5f19d22bc9de3d25681fae31.jpg'),
+                              ),
+                              title: Text(
+                                'Ayasofya Müzesi',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              subtitle: Padding(
+                                  padding: const EdgeInsets.only(top: 5),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      RichText(
+                                        text: TextSpan(
+                                            style: TextStyle(
+                                              color: Colors.grey.shade600,
+                                            ),
+                                            children: [
+                                              TextSpan(
+                                                  text:
+                                                      'Ayasofya veya resmî ismiyle Ayasofya-i Kebîr Câmi-i Şerîfi, eski ismiyle Ayasofya Kilisesi veya Ayasofya Müzesi,'
+                                                      'İstanbul da yer alan bir cami, eski bazilika, katedral ve müze.'),
+                                            ]),
+                                      ),
+                                      SizedBox(height: 5),
+                                      Text(
+                                        'Çalışma Saatleri:24 saat açık',
+                                        style: TextStyle(
+                                            color: Colors.black87,
+                                            fontSize: 13),
+                                      )
+                                    ],
+                                  )),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(),
+                          child: Card(
+                            elevation: 5,
+                            shadowColor: Colors.grey,
+                            color: Colors.white,
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    'https://www.gezilesiyer.com/wp-content/uploads/2018/12/topkapi-sarayi-03.jpg'),
+                              ),
+                              title: Text(
+                                'Topkapı Sarayı',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              subtitle: Padding(
+                                  padding: const EdgeInsets.only(top: 5),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      RichText(
+                                        text: TextSpan(
+                                            style: TextStyle(
+                                              color: Colors.grey.shade600,
+                                            ),
+                                            children: [
+                                              TextSpan(
+                                                  text:
+                                                      'Topkapı Sarayı, İstanbul Sarayburnu nda, Osmanlı İmparatorluğu nun 600 yıllık tarihinin 400 yılı boyunca,'
+                                                      ' devletin idare merkezi olarak kullanılan ve Osmanlı padişahlarının yaşadığı saraydır. '
+                                                      'Bir zamanlar içinde 4.000e yakın insan yaşamıştır..'),
+                                            ]),
+                                      ),
+                                      SizedBox(height: 5),
+                                      Text(
+                                        'Çalışma Saatleri:10:00–16:00',
+                                        style: TextStyle(
+                                            color: Colors.black87,
+                                            fontSize: 13),
+                                      )
+                                    ],
+                                  )),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
