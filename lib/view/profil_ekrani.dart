112806931251eeace527e24fde4b950e78cb4c4c
nuriulgen <bazid04ulgen@gmail.com>
2022-02-20 16:29:27 +0300

    TabBar iözellik ve tasarımı güncellendi
    TabBar body sabitlendi ve tasarım olarak iyileştirildi.

--- lib/view/profil_ekrani.dart
+++ lib/view/profil_ekrani.dart
@@ -17,11 +17,11 @@ class _ProfilSayfasiState extends State<ProfilSayfasi>
   final icon = CupertinoIcons.moon_stars;
   late final TabController _tabController;
   String _logo =
-      'https://cdn.pixabay.com/photo/2016/11/22/23/44/porsche-1851246_960_720.jpg';
+      'https://pbs.twimg.com/profile_images/1408322029061824512/7oNDK2Tb_400x400.jpg';
 
   @override
   void initState() {
-    _tabController = new TabController(length: 3, vsync: this);
+    _tabController = new TabController(length: 2, vsync: this);
 
     super.initState();
   }
@@ -30,250 +30,129 @@ class _ProfilSayfasiState extends State<ProfilSayfasi>
   Widget build(BuildContext context) {
     //final user = UserPreferences.myUser;
     final user = UserPreferences.getUser();
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.more_vert, color: Colors.black, size: 30),
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => Settings()));
-            },
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            'Nuri Ülgen',
+            style: TextStyle(color: Colors.black),
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: ListView(
-          physics: BouncingScrollPhysics(),
-          children: [
-            Row(
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(_logo),
-                  radius: 50,
-                ),
-                const SizedBox(width: 15),
-                Column(
-                  children: [
-                    buildName(user),
-                    const SizedBox(height: 10),
-                    NumbersWidget(),
-                  ],
-                )
-              ],
+          elevation: 0,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.more_vert, color: Colors.black, size: 30),
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => Settings()));
+              },
             ),
-            const SizedBox(height: 15),
-            ElevatedButton(
-              child: Text('Takip Et'),
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.grey,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-              ),
-            ),
-            const SizedBox(height: 15),
-            Container(
-              child: TabBar(
-                labelColor: Colors.black,
-                unselectedLabelColor: Colors.grey,
-                controller: _tabController,
-                tabs: [
-                  Tab(
-                    icon: Icon(
-                      Icons.message,
-                      size: 30,
-                    ),
-                  ),
-                  Tab(
-                    icon: Icon(
-                      CupertinoIcons.star_fill,
-                      size: 30,
-                    ),
-                  ),
-                  Tab(
-                    icon: Icon(
-                      CupertinoIcons.checkmark_square_fill,
-                      size: 30,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              height: 400,
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 10.0,
-                      left: 2.0,
-                      right: 2.0,
-                    ),
+          ],
+        ),
+        body: DefaultTabController(
+          length: 2,
+          child: NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
                     child: Column(
                       children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundImage: NetworkImage(_logo),
+                              radius: 50,
+                            ),
+                            const SizedBox(width: 20),
+                            NumbersWidget(),
+                          ],
+                        ),
+                        const SizedBox(height: 15),
                         Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(25)),
-                          child: Card(
-                            elevation: 5,
-                            shadowColor: Colors.grey,
-                            color: Colors.white,
-                            child: ListTile(
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage(_logo),
-                              ),
-                              title: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    '@Nuri Ulgen',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  Text(
-                                    '18 hour',
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 13,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              subtitle: RichText(
-                                text: TextSpan(
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                    ),
-                                    children: [
-                                      TextSpan(
-                                          text:
-                                              'Contrary to popular belief, Lorem Ipsum is not simply random text.'
-                                              ' It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'),
-                                    ]),
-                              ),
+                          width: 360,
+                          child: ElevatedButton(
+                            child: Text('Takip Et'),
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.grey,
                               shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50),
-                              ),
+                                  borderRadius: BorderRadius.circular(20)),
                             ),
                           ),
                         ),
-                        Container(
-                          decoration: BoxDecoration(),
-                          child: Card(
-                            elevation: 5,
-                            shadowColor: Colors.grey,
-                            color: Colors.white,
-                            child: ListTile(
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage(_logo),
-                              ),
-                              title: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    '@Nuri Ulgen',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  Text(
-                                    '7 minute',
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 13,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              subtitle: RichText(
-                                text: TextSpan(
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                    ),
-                                    children: [
-                                      TextSpan(
-                                          text:
-                                              'There are many variations of passages of Lorem Ipsum available, '
-                                              'but the majority have suffered alteration in some form, by injected humour, or randomised words which.'),
-                                    ]),
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                            ),
+                        const SizedBox(height: 15),
+                      ],
+                    ),
+                  ),
+                ),
+                SliverPersistentHeader(
+                  delegate: _SliverAppBarDelegate(
+                    const TabBar(
+                      labelColor: Colors.black87,
+                      unselectedLabelColor: Colors.grey,
+                      tabs: [
+                        Tab(
+                          icon: Icon(
+                            Icons.message,
+                            size: 30,
                           ),
                         ),
-                        Container(
-                          decoration: BoxDecoration(),
-                          child: Card(
-                            elevation: 5,
-                            shadowColor: Colors.grey,
-                            color: Colors.white,
-                            child: ListTile(
-                              leading: CircleAvatar(
-                                backgroundImage: NetworkImage(_logo),
-                              ),
-                              title: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    '@Nuri Ulgen',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  Text(
-                                    'right now',
-                                    style: TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 13,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              subtitle: RichText(
-                                text: TextSpan(
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                    ),
-                                    children: [
-                                      TextSpan(
-                                          text:
-                                              'Various versions have evolved over the years, sometimes by accident, '
-                                              'sometimes on purpose (injected humour and the like).'),
-                                    ]),
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                            ),
+                        Tab(
+                          icon: Icon(
+                            CupertinoIcons.star_fill,
+                            size: 30,
                           ),
                         ),
                       ],
                     ),
                   ),
-                  Center(child: Text('Oylama')),
-                  Center(child: Text('Eklediğim mekanlar')),
+                  pinned: true,
+                ),
+              ];
+            },
+            body: Container(
+              width: double.infinity,
+              height: 400,
+              child: TabBarView(
+                children: [
+                  // yorumlarım bölümü
+
+                  //favori mekanlar bölümü
                 ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      child: _tabBar,
+      color: Colors.white,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
 
