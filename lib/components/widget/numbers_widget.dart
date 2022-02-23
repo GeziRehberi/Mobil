0822ec2491c4e4decd31d11aa4b635477ec28a47
nuriulgen <bazid04ulgen@gmail.com>
2022-02-23 03:27:05 +0300

    NumbersWidget sınıfı oluşturuldu

---
+++ lib/components/widget/numbers_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class NumbersWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          buildButton(context, '35', 'Takip'),
+          buildDivider(),
+          buildButton(context, '82', 'Takipçiler'),
+        ],
+      );
+  Widget buildDivider() => Container(
+        height: 32,
+        child: VerticalDivider(),
+      );
+
+  Widget buildButton(BuildContext context, String value, String text) =>
+      MaterialButton(
+        padding: EdgeInsets.symmetric(vertical: 4),
+        onPressed: () {},
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              value,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            SizedBox(height: 2),
+            Text(
+              text,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      );
+}
