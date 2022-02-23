bfdb1d21bceeee9b92b597f4f4f9f9a19de854fe
nuriulgen <bazid04ulgen@gmail.com>
2022-02-23 03:28:34 +0300

    Profil düzenleme sayfası güncellendi.
    Profil düzenleme sayfası tasarımı güncellendi.

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/model/user.dart';
@@ -8,6 +9,10 @@ import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
+import '../components/widget/button_widget.dart';
+import '../components/widget/profile_widget.dart';
+import '../components/widget/textfield_widget.dart';
+
 class EditProfilePage extends StatefulWidget {
   @override
   _EditProfilePageState createState() => _EditProfilePageState();
@@ -15,7 +20,7 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   late User user;
-  final icon = CupertinoIcons.moon_stars;
+
   @override
   void initState() {
     super.initState();
@@ -24,221 +29,65 @@ class _EditProfilePageState extends State<EditProfilePage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: BackButton(
-          color: Colors.black,
-        ),
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(icon, color: Colors.black, size: 30),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 32),
-        physics: BouncingScrollPhysics(),
-        children: [
-          ProfileWidget(
-            imagePath: user.imagePath,
-            isEdit: true,
-            onClicked: () async {
-              final image =
-                  await ImagePicker().getImage(source: ImageSource.gallery);
-
-              if (image == null) return;
-
-              final directory = await getApplicationDocumentsDirectory();
-              final name = basename(image.path);
-              final imageFile = File('${directory.path}/$name');
-              final newImage = await File(image.path).copy(imageFile.path);
-
-              setState(() => user = user.copy(imagePath: newImage.path));
-            },
-          ),
-          const SizedBox(height: 24),
-          TextFieldWidget(
-            label: 'Full Name',
-            text: user.name,
-            onChanged: (name) => user = user.copy(name: name),
-          ),
-          const SizedBox(height: 24),
-          TextFieldWidget(
-            label: 'Email',
-            text: user.email,
-            onChanged: (email) => user = user.copy(email: email),
-          ),
-          const SizedBox(height: 24),
-          ButtonWidget(
-            text: 'Save',
-            onClicked: () {
-              UserPreferences.setUser(user);
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ButtonWidget extends StatelessWidget {
-  final String text;
-  final VoidCallback onClicked;
-
-  const ButtonWidget({
-    Key? key,
-    required this.text,
-    required this.onClicked,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          shape: StadiumBorder(),
-          onPrimary: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 32, vertical: 12),
-        ),
-        child: Text(text),
-        onPressed: onClicked,
-      );
-}
-
-class ProfileWidget extends StatelessWidget {
-  final String imagePath;
-  final bool isEdit;
-  final VoidCallback onClicked;
-
-  const ProfileWidget({
-    Key? key,
-    required this.imagePath,
-    this.isEdit = false,
-    required this.onClicked,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme.primary;
-
-    return Center(
-      child: Stack(
-        children: [
-          buildImage(),
-          /* Positioned(
-            bottom: 0,
-            right: 4,
-            child: buildEditIcon(color),
-          ),*/
-        ],
-      ),
-    );
-  }
-
-  Widget buildImage() {
-    final image = imagePath.contains('https://')
-        ? NetworkImage(imagePath)
-        : FileImage(File(imagePath));
-
-    return ClipOval(
-      child: Material(
-        color: Colors.transparent,
-        child: Ink.image(
-          image: image as ImageProvider,
-          fit: BoxFit.cover,
-          width: 128,
-          height: 128,
-          child: InkWell(onTap: onClicked),
-        ),
-      ),
-    );
-  }
-// düzenleme işareti
-/*  Widget buildEditIcon(Color color) => buildCircle(
-        color: Colors.white,
-        all: 3,
-        child: buildCircle(
-          color: color,
-          all: 8,
-          child: Icon(
-            isEdit ? Icons.add_a_photo : Icons.edit,
-            color: Colors.white,
-            size: 20,
-          ),
-        ),
-      );*/
-
-  Widget buildCircle({
-    required Widget child,
-    required double all,
-    required Color color,
-  }) =>
-      ClipOval(
-        child: Container(
-          padding: EdgeInsets.all(all),
-          color: color,
-          child: child,
-        ),
-      );
-}
-
-class TextFieldWidget extends StatefulWidget {
-  final int maxLines;
-  final String label;
-  final String text;
-  final ValueChanged<String> onChanged;
-
-  const TextFieldWidget({
-    Key? key,
-    this.maxLines = 1,
-    required this.label,
-    required this.text,
-    required this.onChanged,
-  }) : super(key: key);
-
-  @override
-  _TextFieldWidgetState createState() => _TextFieldWidgetState();
-}
-
-class _TextFieldWidgetState extends State<TextFieldWidget> {
-  late final TextEditingController controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = TextEditingController(text: widget.text);
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            widget.label,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-          ),
-          const SizedBox(height: 8),
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
+  Widget build(BuildContext context) => ThemeSwitchingArea(
+        child: Builder(
+          builder: (context) => Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back, color: Colors.black, size: 30),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
+              elevation: 0,
             ),
-            maxLines: widget.maxLines,
-            onChanged: widget.onChanged,
+            body: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              physics: BouncingScrollPhysics(),
+              children: [
+                ProfileWidget(
+                  imagePath: user.imagePath,
+                  isEdit: true,
+                  onClicked: () async {
+                    final image = await ImagePicker()
+                        .getImage(source: ImageSource.gallery);
+
+                    if (image == null) return;
+
+                    final directory = await getApplicationDocumentsDirectory();
+                    final name = basename(image.path);
+                    final imageFile = File('${directory.path}/$name');
+                    final newImage =
+                        await File(image.path).copy(imageFile.path);
+
+                    setState(() => user = user.copy(imagePath: newImage.path));
+                  },
+                ),
+                const SizedBox(height: 24),
+                TextFieldWidget(
+                  label: 'Full Name',
+                  text: user.name,
+                  onChanged: (name) => user = user.copy(name: name),
+                ),
+                const SizedBox(height: 24),
+                TextFieldWidget(
+                  label: 'Email',
+                  text: user.email,
+                  onChanged: (email) => user = user.copy(email: email),
+                ),
+                const SizedBox(height: 24),
+                const SizedBox(height: 24),
+                ButtonWidget(
+                  text: 'Save',
+                  onClicked: () {
+                    UserPreferences.setUser(user);
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       );
 }
