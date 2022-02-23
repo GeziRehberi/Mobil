eb6166b1444fb01d3f1af26acfb49c7bf6a1b7c4
nuriulgen <bazid04ulgen@gmail.com>
2022-02-23 03:32:28 +0300

    Theme Provider eklendi
    Theme Provider ile dark mode eklenmesi için temel oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:animated_theme_switcher/animated_theme_switcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_widgets/login_pages/acilis_ekrani.dart';
-import 'package:flutter_widgets/model/user_preferences.dart';
 import 'package:wiredash/wiredash.dart';
+
+import 'model/themes.dart';
+import 'model/user_preferences.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,25 +14,29 @@ Future main() async {
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-
   await UserPreferences.init();
-
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   final _navigatorKey = GlobalKey<NavigatorState>();
+  final user = UserPreferences.getUser();
 
   @override
   Widget build(BuildContext context) {
-    return Wiredash(
-      projectId: 'YOUR-PROJECT-ID',
-      secret: 'YOUR-SECRET',
-      navigatorKey: _navigatorKey,
-      child: MaterialApp(
-        navigatorKey: _navigatorKey,
-        debugShowCheckedModeBanner: false,
-        home: SplashScreen(),
+    return ThemeProvider(
+      initTheme: user.isDarkMode ? MyThemes.darkTheme : MyThemes.lightTheme,
+      child: Builder(
+        builder: (context) => Wiredash(
+          projectId: 'YOUR-PROJECT-ID',
+          secret: 'YOUR-SECRET',
+          navigatorKey: _navigatorKey,
+          child: MaterialApp(
+            navigatorKey: _navigatorKey,
+            debugShowCheckedModeBanner: false,
+            home: SplashScreen(),
+          ),
+        ),
       ),
     );
   }
