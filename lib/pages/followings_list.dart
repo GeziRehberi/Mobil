92e66c41d7a9e5b457ae109d7a4d5eab5382cc74
nuriulgen <bazid04ulgen@gmail.com>
2022-03-13 17:06:34 +0300

    Followings classı oluşturuldu ve arayüze eklendi
    Followings ara yüz de listelendi

---
+++ lib/pages/followings_list.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../components/widget/appBar_widget.dart';
+import '../components/widget/follow_list.dart';
+
+class FollowingsPage extends StatefulWidget {
+  const FollowingsPage({Key? key}) : super(key: key);
+
+  @override
+  _FollowingsPageState createState() => _FollowingsPageState();
+}
+
+class _FollowingsPageState extends State<FollowingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context, 'Takip Edilenler'),
+      body: ListView(children: [
+        FollowList(
+          name: 'Nuri',
+          surName: 'Ulgen',
+          photoPath: '',
+          followTitle: 'Takip Ediliyor',
+        ),
+        FollowList(
+          name: 'Nuri',
+          surName: 'Ulgen',
+          photoPath: '',
+          followTitle: 'Takip Ediliyor',
+        ),
+        FollowList(
+          name: 'Nuri',
+          surName: 'Ulgen',
+          photoPath: '',
+          followTitle: 'Takip Ediliyor',
+        ),
+      ]),
+    );
+  }
+}
