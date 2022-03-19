4cc2b1afd5d2385bcca814498d8e5373922e7e94
nuriulgen <bazid04ulgen@gmail.com>
2022-03-20 00:59:57 +0300

    Giriş Yap ara yüzü güncellendi
    Giriş Yap ara yüzü tasarım ve performans olarak güncellendi

--- lib/view/authentication/login/giris_yap_ekrani.dart
+++ lib/view/authentication/login/giris_yap_ekrani.dart
@@ -3,6 +3,8 @@ import 'package:email_validator/email_validator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../../core/components/form_field.dart';
+import '../../../core/components/widget/icon_language.dart';
 import '../../../core/init/lang/locale_keys.dart';
 import '../../home/bottom_navigator_ekrani.dart';
 import '../forgot/sifremi_unuttum_ekrani.dart';
@@ -27,49 +29,38 @@ class _GirisYapEkraniState extends State<GirisYapEkrani> {
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(left: 16, right: 16, top: 130),
+              padding: const EdgeInsets.only(left: 345, top: 30),
+              child: IconButton(
+                icon: Icon(Icons.language, size: 30),
+                onPressed: () {
+                  languageChangeSettings(context);
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, top: 110),
               child: Center(
-                child: Container(
-                  width: 180,
-                  height: 43,
-                  child: Center(
-                    child: Text(
-                      'Gezi Rehberi',
-                      style: TextStyle(
-                        fontSize: 22,
-                      ),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.teal,
-                    borderRadius: BorderRadius.circular(10),
+                child: Text(
+                  'Gezi Rehberi',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xFF3949AB),
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            Container(
-              child: Image.asset(
-                'assets/konum.png',
-                scale: 1,
-              ),
-            ),
-            SizedBox(height: 20),
-            Form(
-              key: _formkey,
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15),
-                    child: TextFormField(
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Form(
+                key: _formkey,
+                child: Column(
+                  children: <Widget>[
+                    FormFieldText(
+                      left: 30,
+                      right: 30,
                       initialValue: 'nuri@gmail.com',
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
                       keyboardType: TextInputType.emailAddress,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
-                        hintText: LocaleKeys.login_email.tr(),
-                      ),
+                      hintText: LocaleKeys.login_email.tr(),
                       onSaved: (deger) {
                         _email = deger!;
                         print(_email); // verilerin kaydedilmesi
@@ -77,7 +68,7 @@ class _GirisYapEkraniState extends State<GirisYapEkrani> {
                       validator: (deger) {
                         // email doğrulama işlemleri
                         if (deger!.isEmpty) {
-                          return 'mail boş olamaz';
+                          return 'Email boş olamaz!';
                         } else if (!EmailValidator.validate(deger)) {
                           return 'Geçerli bir mail giriniz';
                         } else {
@@ -85,18 +76,14 @@ class _GirisYapEkraniState extends State<GirisYapEkrani> {
                         }
                       },
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        left: 15.0, right: 15.0, top: 15, bottom: 0),
-                    child: TextFormField(
+                    SizedBox(height: 5),
+                    FormFieldText(
+                      left: 30,
+                      right: 30,
                       initialValue: 'nuri07',
                       obscureText: true,
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: LocaleKeys.login_password.tr(),
-                          hintText: 'Şifrenizi Giriniz'),
+                      keyboardType: TextInputType.text,
+                      hintText: LocaleKeys.login_password.tr(),
                       onSaved: (deger) {
                         _sifre = deger!;
                         print(_sifre);
@@ -112,59 +99,62 @@ class _GirisYapEkraniState extends State<GirisYapEkrani> {
                         }
                       },
                     ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (_) => SifremiUnutttumEkrani()));
-                    },
-                    child: Text(
-                      LocaleKeys.login_forgotText.tr(),
-                      style: TextStyle(color: Color(0xFF3949AB), fontSize: 15),
-                    ),
-                  ),
-                  Container(
-                    height: 50,
-                    width: 300,
-                    decoration: BoxDecoration(
-                        color: Color(0xFF3949AB),
-                        borderRadius: BorderRadius.circular(20)),
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: const Color(0xFF3949AB),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25)),
-                      ),
+                    SizedBox(height: 10),
+                    TextButton(
                       onPressed: () {
-                        bool _validate = _formkey.currentState!.validate();
-                        if (_validate) {
-                          _formkey.currentState!.save();
-                          Navigator.of(context).pushReplacement(
-                              CupertinoPageRoute(
-                                  builder: (context) =>
-                                      const BottomNavigatorPage()));
-                        }
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => SifremiUnutttumEkrani()));
                       },
                       child: Text(
-                        LocaleKeys.login_loginButton.tr(),
-                        style: TextStyle(color: Colors.white, fontSize: 25),
+                        LocaleKeys.login_forgotText.tr(),
+                        style:
+                            TextStyle(color: Color(0xFF3949AB), fontSize: 15),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 40),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => KayitOlEkrani()));
-                    },
-                    child: Text(
-                      LocaleKeys.login_dontAccount.tr(),
-                      style: TextStyle(color: Colors.black),
+                    SizedBox(height: 10),
+                    Container(
+                      height: 50,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF3949AB),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color(0xFF3949AB),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                        ),
+                        onPressed: () {
+                          bool _validate = _formkey.currentState!.validate();
+                          if (_validate) {
+                            _formkey.currentState!.save();
+                            Navigator.of(context).pushReplacement(
+                                CupertinoPageRoute(
+                                    builder: (context) =>
+                                        const BottomNavigatorPage()));
+                          }
+                        },
+                        child: Text(
+                          LocaleKeys.login_loginButton.tr(),
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        ),
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 180),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => KayitOlEkrani()));
+                      },
+                      child: Text(
+                        LocaleKeys.login_dontAccount.tr(),
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
