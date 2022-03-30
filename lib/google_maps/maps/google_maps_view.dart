058ee81970908d2b569ca7759164e666d92e66cb
nuriulgen <bazid04ulgen@gmail.com>
2022-03-30 23:19:25 +0300

    Google Maps search bar eklendi
    Google haritalarda arama yapmak için search bar eklendi

--- lib/google_maps/maps/google_maps_view.dart
+++ lib/google_maps/maps/google_maps_view.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -10,13 +13,15 @@ class GoogleMapsView extends StatefulWidget {
 }
 
 class _GoogleMapsViewState extends State<GoogleMapsView> {
-  late GoogleMapController mapController;
+  late String searchAddress;
+  late final Completer<GoogleMapController> _controller = Completer();
   static final CameraPosition _cameraPosition = CameraPosition(
-      //Harita ekranda ilk açıldığında gelen konumunu ayarlanmasını sağlar.
-      // bearing: 192.8334901395799,
-      target: LatLng(41.011006, 28.930903), // haritanın başlama yerini belirler
-      //  tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+    //Harita ekranda ilk açıldığında gelen konumunu ayarlanmasını sağlar.
+    // bearing: 192.8334901395799,
+    target: LatLng(41.011006, 28.930903), // haritanın başlama yerini belirler
+    //  tilt: 59.440717697143555,
+    zoom: 10,
+  );
 
   @override
   void initState() {
@@ -32,13 +37,46 @@ class _GoogleMapsViewState extends State<GoogleMapsView> {
           GoogleMap(
             mapType: MapType.normal, // harita type belirler.
             initialCameraPosition: _cameraPosition,
-            myLocationEnabled: true, // mevcut lokasyonu gösterir.
+            myLocationEnabled: true, // şuanki konumumu gösterir.
+            myLocationButtonEnabled: false,
             // trafficEnabled: true, // trafik durumunu gösterir.
             zoomControlsEnabled: false,
-            onMapCreated: onMapCreated,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+          Positioned(
+            top: 30.0,
+            left: 15.0,
+            right: 15.0,
+            child: Container(
+              height: 50.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white.withOpacity(1),
+              ),
+              child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Adres Giriniz:',
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(left: 20.0),
+                    suffix: IconButton(
+                      icon: Icon(Icons.search),
+                      iconSize: 25.0,
+                      onPressed: () {},
+                    ),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      searchAddress = value;
+                    });
+                  }),
+            ),
           ),
         ],
       ),
     );
-  }
+  } // rgb - 26, 155,231
+
 }
