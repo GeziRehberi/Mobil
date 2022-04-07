c0292b53fa883e470fde977538a1aeb5b4982553
nuriulgen <bazid04ulgen@gmail.com>
2022-04-07 13:55:44 +0300

    Oylama sayfası  oluşturuldu
    Oylama sayfasına tarihi yerin adı ,kategorisi, kaydet butonu, like ve dislike butonları eklendi

--- lib/view/home/oylama_ekrani.dart
+++ lib/view/home/oylama_ekrani.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../core/components/favorite_place.dart';
+import '../../core/components/widget/appBar_widget.dart';
+
 class OylamaSayfasi extends StatefulWidget {
   const OylamaSayfasi({Key? key}) : super(key: key);
 
@@ -11,6 +14,21 @@ class _OylamaSayfasiState extends State<OylamaSayfasi> {
   final _navigatorKey = GlobalKey<NavigatorState>();
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return Scaffold(
+      appBar: buildAppBar(context, ''),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(children: <Widget>[
+          Voting(
+            placeName: 'Galata',
+            categoryName: 'Turistik Yer',
+          ),
+          Voting(
+            placeName: 'Burger King',
+            categoryName: 'Hamburger Resturantı',
+          ),
+        ]),
+      ),
+    );
   }
 }
