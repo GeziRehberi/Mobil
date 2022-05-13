9269e922036399ce6c81974af2b1e291936cbe11
nuriulgen <bazid04ulgen@gmail.com>
2022-05-14 00:51:12 +0300

    Yol tarifi sayfasının backend ve frontend oluşturuldu.
    Şehirler/ülkeler arasında yol tarifi için arayüz oluşturuldu.

--- lib/google_maps/maps/google_maps_view.dart
+++ lib/google_maps/maps/google_maps_view.dart
@@ -5,6 +5,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
+import 'distance.dart';
 import 'location_service.dart';
 
 class GoogleMapsView extends StatefulWidget {
@@ -26,19 +27,42 @@ class _GoogleMapsViewState extends State<GoogleMapsView> {
     zoom: 10,
   );
 
+  List<Marker> _marker = [];
+  List<Marker> _list = [
+    Marker(
+      markerId: MarkerId('1'),
+      position: LatLng(41.025742, 28.974201),
+      infoWindow: InfoWindow(
+        title: 'Galata Kulesi',
+        snippet: 'Tarihi bir mekan ',
+      ),
+    ),
+    Marker(
+      markerId: MarkerId('2'),
+      position: LatLng(41.0164, 28.9706),
+      infoWindow: InfoWindow(
+        title: 'Mısır Çarşısı',
+        snippet: 'Tarihi çarşı',
+      ),
+    ),
+  ];
+
   @override
   void initState() {
     super.initState();
     Geolocator.requestPermission();
+    _marker.addAll(_list);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: Stack(
         children: <Widget>[
           GoogleMap(
             mapType: MapType.normal, // harita type belirler.
+            markers: Set<Marker>.of(_marker),
             initialCameraPosition: _cameraPosition,
             myLocationEnabled: true, // şuanki konumumu gösterir.
             myLocationButtonEnabled: false,
@@ -48,7 +72,7 @@ class _GoogleMapsViewState extends State<GoogleMapsView> {
               _controller.complete(controller);
             },
           ),
-          Positioned(
+          /* Positioned(
             top: 30.0,
             left: 15.0,
             right: 15.0,
@@ -78,15 +102,89 @@ class _GoogleMapsViewState extends State<GoogleMapsView> {
                 ),
               ),
             ),
-          ),
+          ), */
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.white,
-        onPressed: getUserLocation,
-        child: Image.asset(
-          "assets/images/crosshairs-gps.png",
-        ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.white,
+            onPressed: () {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => Distance()));
+            },
+            child: Icon(Icons.directions, color: Colors.blue, size: 30),
+          ),
+          SizedBox(height: 10),
+          FloatingActionButton(
+            backgroundColor: Colors.white,
+            onPressed: getUserLocation,
+            child: Image.asset(
+              "assets/images/crosshairs-gps.png",
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30, top: 5),
+            child: Container(
+              height: 200.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white.withOpacity(1),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 16,
+                      right: 20,
+                      top: 16,
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 290,
+                          child: TextField(
+                            controller: _searchController,
+                            textCapitalization: TextCapitalization.words,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Burada Arayın',
+                              contentPadding:
+                                  EdgeInsets.only(left: 20.0, top: 8),
+                              prefixIcon: Icon(Icons.search, size: 25),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 5),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Color(0xFF284CF2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: IconButton(
+                            onPressed: () async {
+                              var place = await LocationService()
+                                  .getPlace(_searchController.text);
+                              _goToPlace(place);
+                            },
+                            icon:
+                                Icon(Icons.done, color: Colors.white, size: 30),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Center(child: Text('Filtreler'))
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   } // rgb - 26, 155,231
